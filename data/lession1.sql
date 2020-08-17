9c8b7ddf910da29822c0c082ea4c89f34bb81ff7
van dung <vandung030701@gmail.com>
2020-08-17 22:04:25 +0700

    Create table in database

---
+++ data/lession1.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA db_student_cms_plusplus DEFAULT CHARACTER SET utf8mb4;
+USE db_student_cms_plusplus;
+CREATE TABLE table_student(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name varchar(50) NOT NULL,
+mssv varchar(8) NULL,
+password varchar(20) NOT NULL,
+phone varchar(10) NULL,
+address varchar(100) NULL,
+age INT NOT NULL,
+email varchar(30) NOT NULL,
+created_timstamp timestamp DEFAULT current_timestamp,
+last_updated_timestamp timestamp 
+);
+CREATE TABLE table_class (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name varchar(20) NOT NULL,
+major varchar(30) NULL,
+total_student INT NOT NULL,
+teacher_name varchar(30) NOT NULL,
+teacher_phone varchar(10) NULL,
+created_timestamp timestamp default current_timestamp,
+last_updated_timestamp timestamp
+);
+CREATE TABLE table_student_class(
+student_id varchar(10),
+class_id varchar(10) 
+)
