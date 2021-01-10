e80ff4470cf095353137e43767652d90e983b4fa
van dung <vandung030701@gmail.com>
2021-01-10 21:06:00 +0700

    get information user

---
+++ src/main/java/plusplus/spring/lesson31/GetUser.java
@@ -0,0 +1,26 @@
+package plusplus.spring.lesson31;
+
+import org.apache.catalina.realm.UserDatabaseRealm;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class GetUser {
+    @GetMapping(value = "/user")
+    public User getinformationUser() {
+        User user = new User();
+        return user;
+    }
+
+    @GetMapping(value = "/student")
+    public List<User> getliststudent() {
+        User user = new User();
+        List<User> list = new ArrayList<>();
+        list.add(new User("Van", "van032@gmail.com", "145353"));
+        list.add(user);
+        return list;
+    }
+}
---
+++ src/main/java/plusplus/spring/lesson31/User.java
@@ -0,0 +1,16 @@
+package plusplus.spring.lesson31;
+
+public class User {
+    public String name="Dung";
+    public String email="vandung030701@gmail,com";
+     String mssv="1507964";
+
+    public User() {
+    }
+
+    public User(String name, String email, String mssv) {
+        this.name = name;
+        this.email = email;
+        this.mssv = mssv;
+    }
+}
